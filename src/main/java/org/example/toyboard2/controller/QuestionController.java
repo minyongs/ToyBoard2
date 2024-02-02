63e8701d51bdb8a7dd3c3a770ad7df430a6a143b
minyongs <stylish_c@naver.com>
2024-02-02 13:15:36 +0900

    삭제 메서드

--- src/main/java/org/example/toyboard2/controller/QuestionController.java
+++ src/main/java/org/example/toyboard2/controller/QuestionController.java
@@ -6,9 +6,7 @@ import org.example.toyboard2.dto.QuestionDTO;
 import org.example.toyboard2.service.QuestionService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -30,12 +28,38 @@ public class QuestionController {
     @GetMapping("/detail/{id}")
     public String getDetail(Model model, @PathVariable Long id){
 
-        QuestionDTO questionDTO = questionService.getDetail(id);
-        model.addAttribute("questionDetail",questionDTO);
-        return "question_detail";
+        QuestionDTO questionDTO = questionService.getDetail(id); // questionDTO 객체에 상세페이지 정보 넘김
+        model.addAttribute("questionDetail",questionDTO); //questionDetail 이란 객체로 모델에 전달
+        return "question_detail"; //모델이 뷰에게 전달
 
 
     }
+
+    @GetMapping("/post")
+    public String postGet(){
+        return "/post";
+    }
+
+    @PostMapping("/post_question")
+    public String postQuestion(Model model, @ModelAttribute QuestionDTO questionDTO){
+        questionService.postQuestion(questionDTO);
+        return "redirect:/question/list";
+
+    }
+
+
+
+
+
+
+
+    @PostMapping("/delete/{id}")
+    public String delete(@PathVariable Long id){
+        questionService.deleteQuestion(id);
+        return "redirect:/question/list";
+    }
+
+
 
 
 }
