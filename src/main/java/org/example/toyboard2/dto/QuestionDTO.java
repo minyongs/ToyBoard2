bfe56b5633e130210305a8b257accbb164b2b69e
minyongs <stylish_c@naver.com>
2024-02-02 13:16:05 +0900

    필요한 생성자 추가

--- src/main/java/org/example/toyboard2/dto/QuestionDTO.java
+++ src/main/java/org/example/toyboard2/dto/QuestionDTO.java
@@ -2,20 +2,25 @@ package org.example.toyboard2.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.example.toyboard2.entity.Answer;
 import org.example.toyboard2.entity.Question;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @Setter
-@AllArgsConstructor
+@NoArgsConstructor
 public class QuestionDTO {
     private Long id;
     private String title;
     private String content;
 
     private LocalDateTime createdAt;
+    private List<AnswerDTO> answerDTOList; // 하나의 글에 여러개의 댓글이 달릴수 있으므로 AnswerDTO 들을 받을수 있는 리스트 필요
+
 
     public static Question toEntity(QuestionDTO questionDTO){
         Question question = new Question();
@@ -25,6 +30,21 @@ public class QuestionDTO {
         question.setCreatedAt(questionDTO.getCreatedAt());
         return question;
     }
+    public QuestionDTO(Long id,String title,String content,LocalDateTime createdAt){
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.createdAt =createdAt;
+    }
+
+    public QuestionDTO(Long id, String title, String content, LocalDateTime createdAt, List<AnswerDTO> answerList) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.createdAt = createdAt;
+        this.answerDTOList = answerList;
+    }
+
 
 }
 
