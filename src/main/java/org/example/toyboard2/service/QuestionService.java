e2fe5143b5addf85b403c277338d8fcf690f5c43
minyongs <stylish_c@naver.com>
2024-02-02 13:13:56 +0900

    deleteQuestion 추가

--- src/main/java/org/example/toyboard2/service/QuestionService.java
+++ src/main/java/org/example/toyboard2/service/QuestionService.java
@@ -1,6 +1,7 @@
 package org.example.toyboard2.service;
 
 import lombok.RequiredArgsConstructor;
+import org.example.toyboard2.dto.AnswerDTO;
 import org.example.toyboard2.dto.QuestionDTO;
 import org.example.toyboard2.entity.Question;
 import org.example.toyboard2.exception.DataNotFoundException;
@@ -17,7 +18,7 @@ public class QuestionService {
 
     private final QuestionRepository questionRepository;
 
-
+    //get일땐 가져오는 로직이니까 entity -> dto
     public List<QuestionDTO> getList() {
         return questionRepository.findAll().stream()
                 .map(question -> new QuestionDTO(
@@ -28,16 +29,28 @@ public class QuestionService {
                 .collect(Collectors.toList());
     }
 
-    public QuestionDTO getDetail(Long id){
-        Optional<Question> byId = questionRepository.findById(id);
-        if(byId.isPresent()){
-            Question question = byId.get();
-            return new QuestionDTO(question.getId(), question.getTitle(), question.getContent(),question.getCreatedAt());
+    public QuestionDTO getDetail(Long id) {
+        Question question = questionRepository.findById(id)
+                .orElseThrow(() -> new DataNotFoundException("질문을 찾을수 없습니다"));
 
-        }else{
-            throw new DataNotFoundException("질문을 찾을수 없습니다");
-        }
+        List<AnswerDTO> answerDTOList = question.getAnswer().stream()
+                .map(answer -> new AnswerDTO(answer.getContent(), answer.getCreatedAt()))
+                .collect(Collectors.toList());
+
+        return new QuestionDTO(question.getId(), question.getTitle(), question.getContent(),
+                question.getCreatedAt(), answerDTOList);
+    }
+
+
+    public void postQuestion(QuestionDTO questionDTO){
+        Question question = QuestionDTO.toEntity(questionDTO);
+        questionRepository.save(question);
 
     }
+
+    public void deleteQuestion(Long id){
+        questionRepository.deleteById(id);
+    }
+
 
 }
