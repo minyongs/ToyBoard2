706df119268eadb55aeb1be806c20d55a36ee569
minyongs <stylish_c@naver.com>
2024-02-05 16:27:30 +0900

    회원 서비스

---
+++ src/main/java/org/example/toyboard2/service/UserService.java
@@ -0,0 +1,29 @@
+package org.example.toyboard2.service;
+
+
+
+import org.example.toyboard2.entity.SiteUser;
+import org.example.toyboard2.repository.SiteUserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    private final SiteUserRepository SiteuserRepository;
+    private final PasswordEncoder passwordEncoder;
+
+
+    public SiteUser create(String username, String email, String password) {
+        SiteUser user = new SiteUser();
+        user.setUsername(username);
+        user.setEmail(email);
+        user.setPassword(passwordEncoder.encode(password));
+        SiteuserRepository.save(user);
+        return user;
+    }
+}
