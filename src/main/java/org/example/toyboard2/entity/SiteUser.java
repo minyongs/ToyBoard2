7aa2ba58b1a7e44078dc99b24c7764647be72324
minyongs <stylish_c@naver.com>
2024-02-05 16:27:52 +0900

    유저 엔티티

---
+++ src/main/java/org/example/toyboard2/entity/SiteUser.java
@@ -0,0 +1,26 @@
+package org.example.toyboard2.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+public class SiteUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+
+    @Column(unique = true)
+    private String email;
+}
