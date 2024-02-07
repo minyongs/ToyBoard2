57251ea00380018fdcd2330c042a50a555720bf0
minyongs <stylish_c@naver.com>
2024-02-07 13:43:31 +0900

    findByUsername 메서드 추가

--- src/main/java/org/example/toyboard2/repository/SiteUserRepository.java
+++ src/main/java/org/example/toyboard2/repository/SiteUserRepository.java
@@ -3,6 +3,8 @@ package org.example.toyboard2.repository;
 import org.example.toyboard2.entity.SiteUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SiteUserRepository extends JpaRepository<SiteUser, Long> {
+import java.util.Optional;
 
+public interface SiteUserRepository extends JpaRepository<SiteUser, Long> {
+    Optional<SiteUser> findByUsername(String username);
 }
