06c250339784fe069d258f149694c4c4cb921f22
minyongs <stylish_c@naver.com>
2024-02-07 13:42:20 +0900

    테스트 데이터 추가

--- src/test/java/org/example/toyboard2/ToyBoard2ApplicationTests.java
+++ src/test/java/org/example/toyboard2/ToyBoard2ApplicationTests.java
@@ -1,13 +1,27 @@
 package org.example.toyboard2;
 
+import org.example.toyboard2.dto.QuestionDTO;
+import org.example.toyboard2.service.QuestionService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
 
 @SpringBootTest
 class ToyBoard2ApplicationTests {
-
+    @Autowired
+    private QuestionService questionService ;
     @Test
-    void contextLoads() {
+    void testJpa() {
+        for (Long i = 1L; i <= 20; i++) {
+            QuestionDTO questionDTO = new QuestionDTO(i,"테스트 데이터입니다 "+i,"ㅎㅎ", LocalDateTime.now());
+            questionService.postQuestion(questionDTO);
+
+        }
+
+
     }
+
 
 }
