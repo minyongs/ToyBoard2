55d8047df6478276657814a58a9339d952e2d62b
minyongs <stylish_c@naver.com>
2024-02-13 14:05:51 +0900

    답변 서비스 수정

--- src/main/java/org/example/toyboard2/service/AnswerService.java
+++ src/main/java/org/example/toyboard2/service/AnswerService.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.example.toyboard2.dto.QuestionDTO;
 import org.example.toyboard2.entity.Answer;
 import org.example.toyboard2.entity.Question;
+import org.example.toyboard2.entity.SiteUser;
 import org.example.toyboard2.repository.AnswerRepository;
 import org.example.toyboard2.repository.QuestionRepository;
 import org.springframework.stereotype.Service;
@@ -19,19 +20,35 @@ public class AnswerService {
     private final QuestionRepository questionRepository;
 
     //post -> repository에 저장하는 로직!
-    public void create(QuestionDTO questionDTO, String content) {
-        // Question 엔티티 조회
-        Question question = questionRepository.findById(questionDTO.getId())
-                .orElseThrow(() -> new EntityNotFoundException("해당 id의 질문이 없습니다: " + questionDTO.getId()));
+//    public void create(QuestionDTO questionDTO, String content) {
+//        // Question 엔티티 조회
+//        Question question = getQuestion(questionDTO);
+//
+//        // Answer 객체 생성 및 설정
+//        Answer answer = new Answer();
+//        answer.setContent(content);
+//        answer.setCreatedAt(LocalDateTime.now());
+//        answer.setQuestion(question);
+//
+//        // Answer 객체 저장
+//        answerRepository.save(answer);
+//    }
 
-        // Answer 객체 생성 및 설정
-        Answer answer = new Answer();
-        answer.setContent(content);
-        answer.setCreatedAt(LocalDateTime.now());
-        answer.setQuestion(question);
+    private Question getQuestion(Long id) {
+        return  questionRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("해당 id의 질문이 없습니다: "));
 
-        // Answer 객체 저장
+    }
+
+    public void create(Long id, String content, SiteUser author){
+
+        Question question = getQuestion(id);
+
+        Answer answer = new Answer(content, question,author);
+
         answerRepository.save(answer);
+
+
     }
 
 }
