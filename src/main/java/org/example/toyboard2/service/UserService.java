c2e5cf5fbaa0e463ce4437b7c26cd51b31fab30b
minyongs <stylish_c@naver.com>
2024-02-13 14:06:02 +0900

    사용자 서비스 수정

--- src/main/java/org/example/toyboard2/service/UserService.java
+++ src/main/java/org/example/toyboard2/service/UserService.java
@@ -4,6 +4,7 @@ package org.example.toyboard2.service;
 
 import org.example.toyboard2.dto.SiteUserDTO;
 import org.example.toyboard2.entity.SiteUser;
+import org.example.toyboard2.exception.DataNotFoundException;
 import org.example.toyboard2.repository.SiteUserRepository;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -11,12 +12,15 @@ import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
 
+import java.util.Optional;
+
 @RequiredArgsConstructor
 @Service
 public class UserService {
 
-    private final SiteUserRepository SiteuserRepository;
+    private final SiteUserRepository siteUserRepository;
     private final PasswordEncoder passwordEncoder;
+
 
 
     public void create(SiteUserDTO siteUserDTO) {
@@ -24,7 +28,18 @@ public class UserService {
         user.setUsername(siteUserDTO.getUserName());
         user.setEmail(siteUserDTO.getEmail());
         user.setPassword(passwordEncoder.encode(siteUserDTO.getPassword()));
-        SiteuserRepository.save(user);
+        siteUserRepository.save(user);
 
     }
-}
+
+    public SiteUser getUser(String username){
+        Optional<SiteUser> siteUser = siteUserRepository.findByUsername(username);
+        if (siteUser.isPresent()) {
+            return siteUser.get();
+        } else {
+            throw new DataNotFoundException("사용자를 찾을 수 없습니다");
+        }
+    }
+
+    }
+
