9c957ef60be453a5e827d338a30e5550ef6d054b
minyongs <stylish_c@naver.com>
2024-02-13 14:05:40 +0900

    컨트롤러 수정

--- src/main/java/org/example/toyboard2/controller/AnswerController.java
+++ src/main/java/org/example/toyboard2/controller/AnswerController.java
@@ -4,8 +4,11 @@ package org.example.toyboard2.controller;
 import lombok.RequiredArgsConstructor;
 import org.example.toyboard2.dto.QuestionDTO;
 
+import org.example.toyboard2.entity.SiteUser;
 import org.example.toyboard2.service.AnswerService;
 import org.example.toyboard2.service.QuestionService;
+import org.example.toyboard2.service.UserService;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,14 +25,16 @@ public class AnswerController {
 
     private final QuestionService questionService;
     private final AnswerService answerService;
+    private final UserService userService;
 
 
-
+    @PreAuthorize("isAuthenticated()")
     @PostMapping("/create/{id}")
     public String createAnswer(Model model, @PathVariable Long id, @RequestParam(value = "content") String content, Principal principal) {
-        Long id2 = questionService.getDetail(id).getId(); // Question dto 를 id 값으로 찾아옴
+        Long id2 = questionService.getDetail(id).getId();
+        SiteUser user = userService.getUser(principal.getName());
 
-        answerService.create(id2, content); // 답변 생성
+        answerService.create(id2, content,user); // 답변 생성
         return String.format("redirect:/question/detail/%s", id);
     }
 
