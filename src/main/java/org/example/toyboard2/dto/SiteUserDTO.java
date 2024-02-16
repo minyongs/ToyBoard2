259b2f82b20cd147757fd8a31bc6710219bf1e8d
minyongs <stylish_c@naver.com>
2024-02-16 15:16:51 +0900

    유효성 추가

--- src/main/java/org/example/toyboard2/dto/SiteUserDTO.java
+++ src/main/java/org/example/toyboard2/dto/SiteUserDTO.java
@@ -13,11 +13,12 @@ import org.hibernate.engine.internal.ImmutableEntityEntry;
 @Setter
 public class SiteUserDTO {
 
-    @Size(min = 3, max = 25)
+    @Size(min = 3, max = 25, message = "아이디는 3~25자여야 합니다.")
     @NotEmpty(message = "ID는 필수 입력 사항입니다.")
     private String userName;
 
 
+    @Size(min = 3, max =10,message = "비밀번호는 3~10자 입니다.")
     @NotEmpty(message = "비밀번호는 필수입력 사항입니다.")
     private String password;
 
