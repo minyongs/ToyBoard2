6f02a81cd24336dbe72acd8eccf4c1118862bc41
minyongs <stylish_c@naver.com>
2024-02-22 16:32:50 +0900

    userName 을 통해 user가 작성한 글을 전체 조회

--- src/main/java/org/example/toyboard2/repository/QuestionRepository.java
+++ src/main/java/org/example/toyboard2/repository/QuestionRepository.java
@@ -1,18 +1,28 @@
 package org.example.toyboard2.repository;
 
+import jakarta.persistence.LockModeType;
+import org.example.toyboard2.dto.QuestionDTO;
 import org.example.toyboard2.entity.Question;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+
+import java.util.List;
 
 
 public interface QuestionRepository extends JpaRepository<Question, Long> {
 
     // 모든 질문을 페이징 처리하여 조회
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     Page<Question> findAll(Pageable pageable);
 
     // 제목에 특정 키워드가 포함된 질문만 페이징 처리하여 조회
     Page<Question> findByTitleContaining(String title, Pageable pageable);
+
+
+    List<Question> findAllByAuthorUsername(String username);
+
 
 
 }
